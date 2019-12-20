cbcbb25cfdb55903c8da421b063063a690e8e00b
Tyler Filla <tylerfilla@hotmail.com>
2019-12-20 12:59:35 -0600

    Solid background

--- rasterizer/lesson2/src/main.c
+++ rasterizer/lesson2/src/main.c
@@ -10,7 +10,54 @@
 //
 
 #include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+/** An RGBA color. */
+typedef union {
+  struct {
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+    uint8_t a;
+  };
+  uint32_t rgba;
+} color_t;
+
+/** A simple image. */
+typedef struct {
+  int width;
+  int height;
+  color_t* pixels;
+} image_t;
+
+/** Access a pixel in an image. */
+#define image_pixel(image, x, y) \
+    (((image_t*) (image))->pixels[x + y * ((image_t*) (image))->width])
 
 int main(void) {
-  printf("Hello, rasterizer lesson 2!\n");
+  // Allocate output image
+  image_t image;
+  image.width = 512;
+  image.height = 512;
+  image.pixels = calloc(image.width * image.height, sizeof(color_t));
+
+  // Add background fill
+  for (int x = 0; x < image.width; ++x) {
+    for (int y = 0; y < image.height; ++y) {
+      image_pixel(&image, x, y) = (color_t) {.r = 80, .g = 80, .b = 140, .a = 255};
+    }
+  }
+
+  // Try to write the output image
+  if (!stbi_write_png("output.png", image.width, image.height, 4, image.pixels, 0)) {
+    fprintf(stderr, "error: failed to write output image\n");
+    return 1;
+  }
+
+  // Clean up image pixels
+  free(image.pixels);
 }
