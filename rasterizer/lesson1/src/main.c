3abd616a180b8e47b26e7ac75de7768c6a67f9c7
Tyler Filla <tylerfilla@hotmail.com>
2019-12-19 20:31:13 -0600

    Work on PNG abstraction

--- rasterizer/lesson1/src/main.c
+++ rasterizer/lesson1/src/main.c
@@ -7,45 +7,106 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
-#include <time.h>
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
-typedef struct point {
-  float x;
-  float y;
-} point_t;
+// An RGBA color
+typedef union color {
+  struct {
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+    uint8_t a;
+  };
+  uint32_t value;
+} color_t;
 
-static void line(uint32_t* image, int width, const point_t* p0, const point_t* p1, uint32_t color) {
-  for (float t = 0; t < 1.0f; t += 0.01f) {
-    int x = (int) (p0->x + (p1->x - p0->x) * t);
-    int y = (int) (p0->y + (p1->y - p0->y) * t);
-    image[x + y * width] = color;
+// A PNG image
+typedef struct png {
+  int width;
+  int height;
+  color_t* pixels;
+} png_t;
+
+// Construction parameters for a PNG image
+typedef struct png_params {
+  int width;
+  int height;
+} png_params_t;
+
+// Create a PNG image instance
+#define png_create(params) \
+    ((png_t*) png_construct((png_t*) malloc(sizeof(png_t)), (const png_params_t*) (params)))
+
+// Destroy a PNG image instance
+#define png_destroy(self) \
+    ((void) free(png_destruct((png_t*) (self))))
+
+// Construct a PNG image instance
+static png_t* png_construct(png_t* self, const png_params_t* params) {
+  self->width = params->width;
+  self->height = params->height;
+  self->pixels = calloc(params->width * params->height, sizeof(color_t));
+  return self;
+}
+
+// Destruct a PNG image instance
+static png_t* png_destruct(png_t* self) {
+  free(self->pixels);
+  return self;
+}
+
+// Get a pixel in a PNG image
+static color_t png_pixel_get(const png_t* self, int x, int y) {
+  return self->pixels[x + y * self->width];
+}
+
+// Set a pixel in a PNG image
+static void png_pixel_set(png_t* self, int x, int y, color_t color) {
+  self->pixels[x + y * self->width] = color;
+}
+
+// Write a PNG image
+static int png_write(const png_t* self, const char* filename) {
+  return !stbi_write_png(filename, self->width, self->height, 4, self->pixels, 0);
+}
+
+// Draw a simple line
+static void line(png_t* image, int x0, int y0, int x1, int y1, color_t color) {
+  for (int x = x0; x <= x1; ++x) {
+    float t = (float) (x - x0) / (float) (x1 - x0);
+    int y = (int) ((float) y0 + (float) (y1 - y0) * t);
+    png_pixel_set(image, x, y, color);
   }
 }
 
 int main(void) {
-  srand((unsigned int) time(NULL));
+  // Create image for output
+  png_t* image;
+  {
+    png_params_t params;
+    params.width = 256;
+    params.height = 256;
+    image = png_create(&params);
+  }
 
-  const int image_width = 256;
-  const int image_height = 256;
-  uint32_t* image_data = calloc(image_width * image_height, sizeof(uint32_t));
-
-  for (int x = 0; x < image_width; ++x) {
-    for (int y = 0; y < image_height; ++y) {
-      image_data[x + y * image_width] = (0xff << 24) // alpha
-          | ((rand() % 0x7f + 0x7e) << 16) // blue
-          | ((rand() % 0x7f + 0x7e) << 8) // green
-          | ((rand() % 0x7f + 0x7e) << 0); // red
+  // Fill background with a solid color
+  for (int x = 0; x < image->width; ++x) {
+    for (int y = 0; y < image->height; ++y) {
+      png_pixel_set(image, x, y, (color_t) {.r = 255, .g = 255, .b = 255, .a = 255});
     }
   }
 
-  line(image_data, image_width, &(point_t) {20,40}, &(point_t) {50,100}, 0xff000000);
+  // Draw a line
+  line(image, 20, 50, 123, 240, (color_t) {.a = 255});
 
-  if (!stbi_write_png("output.png", image_width, image_height, 4, image_data, 0)) {
+  // Save the output image
+  if (png_write(image, "output.png")) {
     fprintf(stderr, "failed to write output image\n");
+    return 1;
   }
 
-  free(image_data);
+  // Clean up
+  png_destroy(image);
 }
