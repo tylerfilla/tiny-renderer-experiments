f5231fac80c3e71f82a8ef59bfa8c146cf73faf8
Tyler Filla <tylerfilla@hotmail.com>
2019-12-21 03:32:49 -0600

    Flat shading!!!

--- rasterizer/lesson2/src/main.c
+++ rasterizer/lesson2/src/main.c
@@ -9,6 +9,7 @@
 // Rasterizer - Lesson 2
 //
 
+#include <math.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -35,49 +36,52 @@ typedef struct {
 #define image_pixel(image, x, y) \
     (((image_t*) (image))->pixels[x + y * ((image_t*) (image))->width])
 
-/** 2-vector with integer components. */
-typedef struct {
-  int x;
-  int y;
-} vec2i_t;
-
-/** 2-vector with floating point components. */
+/** A 2-vector. */
 typedef struct {
   float x;
   float y;
-} vec2f_t;
+} vec2_t;
 
-/** Convert vec2f_t to vec2i_t. */
-inline static vec2i_t vec2f2i(vec2f_t v) {
-  return (vec2i_t) {
-      .x = (int) v.x,
-      .y = (int) v.y,
-  };
-}
-
-/** 3-vector with integer components. */
-typedef struct {
-  int x;
-  int y;
-  int z;
-} vec3i_t;
-
-/** 3-vector with floating point components. */
+/** A 3-vector. */
 typedef struct {
   float x;
   float y;
   float z;
-} vec3f_t;
+} vec3_t;
+
+/** Dot product between two 3-vectors. */
+inline static float dot3(vec3_t a, vec3_t b) {
+  return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
+/** Cross product between two 3-vectors. */
+inline static vec3_t cross3(vec3_t a, vec3_t b) {
+  return (vec3_t) {
+    .x = a.y * b.z - a.z * b.y,
+    .y = a.z * b.x - a.x * b.z,
+    .z = a.x * b.y - a.y * b.x,
+  };
+}
+
+/** Normalize a 3-vector. */
+inline static vec3_t norm3(vec3_t v) {
+  float mag = sqrtf(dot3(v, v));
+  return (vec3_t) {
+    .x = v.x / mag,
+    .y = v.y / mag,
+    .z = v.z / mag,
+  };
+}
 
 /** Fill a triangle. */
-static void triangle(image_t* image, vec2i_t a, vec2i_t b, vec2i_t c, color_t color) {
+static void triangle(image_t* image, vec2_t a, vec2_t b, vec2_t c, color_t color) {
   // Points that define the on-screen part of the bounding box
   // This is the tightest visible axis-aligned bounding box containing points A, B, and C
-  vec2i_t bbox1 = {
+  vec2_t bbox1 = {
     .x = max(0, min(a.x, min(b.x, c.x))),
     .y = max(0, min(a.y, min(b.y, c.y))),
   };
-  vec2i_t bbox2 = {
+  vec2_t bbox2 = {
     .x = min(image->width, max(a.x, max(b.x, c.x))),
     .y = min(image->height, max(a.y, max(b.y, c.y))),
   };
@@ -85,24 +89,24 @@ static void triangle(image_t* image, vec2i_t a, vec2i_t b, vec2i_t c, color_t co
   // The tutorial explanation felt a little wonky IMHO, so I solve the system using Cramer's rule here
 
   // Vector from A to B
-  vec2i_t ab = {
+  vec2_t ab = {
     .x = b.x - a.x,
     .y = b.y - a.y,
   };
 
   // Vector from A to C
-  vec2i_t ac = {
+  vec2_t ac = {
     .x = c.x - a.x,
     .y = c.y - a.y,
   };
 
   // Iterate over all pixels in the bounding box
-  for (int x = bbox1.x; x < bbox2.x; ++x) {
-    for (int y = bbox1.y; y < bbox2.y; ++y) {
+  for (int x = (int) floorf(bbox1.x); x < (int) ceilf(bbox2.x); ++x) {
+    for (int y = (int) floorf(bbox1.y); y < (int) ceilf(bbox2.y); ++y) {
       // Vector from A to (x, y)
-      vec2i_t ap = {
-        .x = x - a.x,
-        .y = y - a.y,
+      vec2_t ap = {
+        .x = (float) x - a.x,
+        .y = (float) y - a.y,
       };
 
       // Common denominator (see Cramer's rule)
@@ -138,12 +142,12 @@ int main(void) {
   // Vertex position data
   size_t positions_capacity = 10;
   size_t positions_size = 0;
-  vec3f_t* positions = malloc(positions_capacity * sizeof(vec3f_t));
+  vec3_t* positions = malloc(positions_capacity * sizeof(vec3_t));
 
   // Face data
   size_t faces_capacity = 10;
   size_t faces_size = 0;
-  vec3i_t* faces = malloc(faces_capacity * sizeof(vec3i_t));
+  vec3_t* faces = malloc(faces_capacity * sizeof(vec3_t));
 
   // Open up our model file for read
   FILE* file = fopen("data/african_head.obj", "r");
@@ -162,13 +166,13 @@ int main(void) {
       // This line encodes a position vector
 
       // Parse the line
-      vec3f_t position;
+      vec3_t position;
       sscanf(line, "v %f %f %f", &position.x, &position.y, &position.z);
 
       // Store the parsed data
       if (positions_size == positions_capacity) {
         positions_capacity *= 2;
-        positions = realloc(positions, positions_capacity * sizeof(vec3f_t));
+        positions = realloc(positions, positions_capacity * sizeof(vec3_t));
       }
       positions[positions_size] = position;
       positions_size++;
@@ -177,13 +181,13 @@ int main(void) {
 
       // Parse the line
       int _;
-      vec3i_t face;
-      sscanf(line, "f %d/%d/%d %d/%d/%d %d/%d/%d", &face.x, &_, &_, &face.y, &_, &_, &face.z, &_, &_);
+      vec3_t face;
+      sscanf(line, "f %f/%f/%f %f/%f/%f %f/%f/%f", &face.x, &_, &_, &face.y, &_, &_, &face.z, &_, &_);
 
       // Store the parsed data
       if (faces_size == faces_capacity) {
         faces_capacity *= 2;
-        faces = realloc(faces, faces_capacity * sizeof(vec3f_t));
+        faces = realloc(faces, faces_capacity * sizeof(vec3_t));
       }
       faces[faces_size] = face;
       faces_size++;
@@ -195,37 +199,60 @@ int main(void) {
 
   // Iterate over triangular faces in model
   for (int i = 0; i < faces_size; ++i) {
-    vec3i_t face = faces[i];
+    vec3_t face = faces[i];
 
     // Look up vertex positions in 3D space
     // Keep in mind that OBJ files are one-indexed :(
-    vec3f_t p1 = positions[face.x - 1];
-    vec3f_t p2 = positions[face.y - 1];
-    vec3f_t p3 = positions[face.z - 1];
+    vec3_t p1 = positions[(int) face.x - 1];
+    vec3_t p2 = positions[(int) face.y - 1];
+    vec3_t p3 = positions[(int) face.z - 1];
+
+    // The vector from P1 to P2
+    vec3_t p1p2 = {
+      .x = p2.x - p1.x,
+      .y = p2.y - p1.y,
+      .z = p2.z - p1.z,
+    };
+
+    // The vector from P1 to P3
+    vec3_t p1p3 = {
+      .x = p3.x - p1.x,
+      .y = p3.y - p1.y,
+      .z = p3.z - p1.z,
+    };
+
+    // The normalized normal vector to the face
+    vec3_t normal = norm3(cross3(p1p2, p1p3));
 
     // Project these vertices into our 2D screen space
     // This is naive just like in lesson 1 (we just drop the Z-axis altogether!)
-    vec2f_t p1_screen = {
+    vec2_t p1_screen = {
       .x = (1.0f + p1.x) * (float) image.width * 0.5f,
       .y = (1.0f - p1.y) * (float) image.height * 0.5f,
     };
-    vec2f_t p2_screen = {
+    vec2_t p2_screen = {
       .x = (1.0f + p2.x) * (float) image.width * 0.5f,
       .y = (1.0f - p2.y) * (float) image.height * 0.5f,
     };
-    vec2f_t p3_screen = {
+    vec2_t p3_screen = {
       .x = (1.0f + p3.x) * (float) image.width * 0.5f,
       .y = (1.0f - p3.y) * (float) image.height * 0.5f,
     };
 
-    // Draw the transformed triangle to the output image
-    // The great thing about triangles is that they stay triangles even after a mathematical shakedown
-    triangle(&image, vec2f2i(p1_screen), vec2f2i(p2_screen), vec2f2i(p3_screen), (color_t) {
-      .r = rand() % 255,
-      .g = rand() % 255,
-      .b = rand() % 255,
-      .a = 255,
-    });
+    // Compute lighting intensity with a forward lamp
+    float lighting = dot3(normal, (vec3_t) {.x = 0, .y = 0, .z = 1});
+
+    // If the triangle is forward-facing
+    if (lighting > 0) {
+      // Draw the transformed triangle to the output image
+      // The great thing about triangles is that they stay triangles even after a mathematical shakedown
+      triangle(&image, p1_screen, p2_screen, p3_screen, (color_t) {
+        .r = lighting * 255,
+        .g = lighting * 255,
+        .b = lighting * 255,
+        .a = 255,
+      });
+    }
   }
 
   // Clean up model data
@@ -233,7 +260,7 @@ int main(void) {
   free(positions);
 
   // Try to write the output image
-  if (!stbi_write_png("output4.png", image.width, image.height, 4, image.pixels, 0)) {
+  if (!stbi_write_png("output5.png", image.width, image.height, 4, image.pixels, 0)) {
     fprintf(stderr, "error: failed to write output image\n");
     return 1;
   }
