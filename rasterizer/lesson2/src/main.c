839d94a73e90c21c499be24d066d0916ea9126e4
Tyler Filla <tylerfilla@hotmail.com>
2019-12-20 23:46:28 -0600

    Barycentric coordinates! :ghost:

--- rasterizer/lesson2/src/main.c
+++ rasterizer/lesson2/src/main.c
@@ -36,11 +36,108 @@ typedef struct {
 #define image_pixel(image, x, y) \
     (((image_t*) (image))->pixels[x + y * ((image_t*) (image))->width])
 
-/** An integer 2-vector. */
+/** 2-vector with integer components. */
 typedef struct {
   int x;
   int y;
 } vec2i_t;
+
+/** 2-vector with floating point components. */
+typedef struct {
+  float x;
+  float y;
+} vec2f_t;
+
+/** Convert vec2i_t to vec2f_t. */
+inline static vec2f_t vec2i2f(vec2i_t v) {
+  return (vec2f_t) {
+    .x = (float) v.x,
+    .y = (float) v.y,
+  };
+}
+
+/** Convert vec2f_t to vec2i_t. */
+inline static vec2i_t vec2f2i(vec2f_t v) {
+  return (vec2i_t) {
+    .x = (int) v.x,
+    .y = (int) v.y,
+  };
+}
+
+/** 3-vector with integer components. */
+typedef struct {
+  int x;
+  int y;
+  int z;
+} vec3i_t;
+
+/** 3-vector with floating point components. */
+typedef struct {
+  float x;
+  float y;
+  float z;
+} vec3f_t;
+
+/** Convert vec3i_t to vec3f_t. */
+inline static vec3f_t vec3i2f(vec3i_t v) {
+  return (vec3f_t) {
+    .x = (float) v.x,
+    .y = (float) v.y,
+    .z = (float) v.z,
+  };
+}
+
+/** Convert vec3f_t to vec3i_t. */
+inline static vec3i_t vec3f2i(vec3f_t v) {
+  return (vec3i_t) {
+    .x = (int) v.x,
+    .y = (int) v.y,
+    .z = (int) v.z,
+  };
+}
+
+/** Truncate vec3i_t to vec2i_t. */
+inline static vec2i_t trunc3i2(vec3i_t v) {
+  return (vec2i_t) {
+      .x = v.x,
+      .y = v.y,
+  };
+}
+
+/** Pad vec2i_t to vec3i_t. */
+inline static vec3i_t pad2i3(vec2i_t v, int z) {
+  return (vec3i_t) {
+      .x = v.x,
+      .y = v.y,
+      .z = z,
+  };
+}
+
+/** Truncate vec3f_t to vec2f_t. */
+inline static vec2f_t trunc3f2(vec3f_t v) {
+  return (vec2f_t) {
+    .x = v.x,
+    .y = v.y,
+  };
+}
+
+/** Pad vec2f_t to vec3f_t. */
+inline static vec3f_t pad2f3(vec2f_t v, float z) {
+  return (vec3f_t) {
+      .x = v.x,
+      .y = v.y,
+      .z = z,
+  };
+}
+
+/** Cross product between two 3-vectors with floating point components. */
+inline static vec3f_t cross3f(vec3f_t a, vec3f_t b) {
+  return (vec3f_t) {
+    .x = a.y * b.z - a.z * b.y,
+    .y = a.z * b.x - a.x * b.z,
+    .z = a.x * b.y - a.y * b.x,
+  };
+}
 
 /** Draw an aliased line. */
 static void line(image_t* image, vec2i_t a, vec2i_t b, color_t color) {
@@ -87,59 +184,53 @@ static void line(image_t* image, vec2i_t a, vec2i_t b, color_t color) {
 
 /** Fill a triangle. */
 static void triangle(image_t* image, vec2i_t a, vec2i_t b, vec2i_t c, color_t color) {
-  // Sort the points A, B, and C by y-coordinates increasing
-  // This lets us make some nice assumptions later on when filling the triangle line-by-line
-  if (a.y > b.y) {
-    vec2i_t tmp = a;
-    a = b;
-    b = tmp;
-  }
-  if (b.y > c.y) {
-    vec2i_t tmp = b;
-    b = c;
-    c = tmp;
-  }
-  if (a.y > b.y) {
-    vec2i_t tmp = a;
-    a = b;
-    b = tmp;
-  }
+  // Points that define the on-screen part of the bounding box
+  // This is the tightest visible axis-aligned bounding box containing points A, B, and C
+  vec2i_t bbox1 = {
+    .x = max(0, min(a.x, min(b.x, c.x))),
+    .y = max(0, min(a.y, min(b.y, c.y))),
+  };
+  vec2i_t bbox2 = {
+    .x = min(image->width, max(a.x, max(b.x, c.x))),
+    .y = min(image->height, max(a.y, max(b.y, c.y))),
+  };
 
-  // Iterate over the height of the triangle
-  // Since we did the sort above, we just need to scan from point A to point C on the y-axis
-  for (int y = a.y; y < c.y; ++y) {
-    // The x-span at this y-coordinate
-    int x0;
-    int x1;
+  // The tutorial explanation felt a little wonky IMHO, so I solve the system using Cramer's rule here
 
-    // If we are below point B
-    if (y < b.y) {
-      // We are in the lower half of the triangle
-      // We need to solve the AB and CA lines and fill between them
+  // Vector from A to B
+  vec2i_t ab = {
+    .x = b.x - a.x,
+    .y = b.y - a.y,
+  };
 
-      // Calculate parametric "progress" along the AB and CA lines
-      float ab_t = (float) (y - a.y) / (float) (b.y - a.y);
-      float ca_t = (float) (y - a.y) / (float) (c.y - a.y); // meow
+  // Vector from A to C
+  vec2i_t ac = {
+    .x = c.x - a.x,
+    .y = c.y - a.y,
+  };
 
-      // Calculate the x-values we must have on the AB and CA lines
-      x0 = (int) ((float) a.x + ab_t * (float) (b.x - a.x));
-      x1 = (int) ((float) a.x + ca_t * (float) (c.x - a.x));
-    } else {
-      // We are in the upper half of the triangle
-      // We need to solve the BC and CA lines and fill between them
+  // Iterate over all pixels in the bounding box
+  for (int x = bbox1.x; x < bbox2.x; ++x) {
+    for (int y = bbox1.y; y < bbox2.y; ++y) {
+      // Vector from A to (x, y)
+      vec2i_t ap = {
+        .x = x - a.x,
+        .y = y - a.y,
+      };
 
-      // Calculate parametric "progress" along the BC and CA lines
-      float bc_t = (float) (y - b.y) / (float) (c.y - b.y);
-      float ca_t = (float) (y - a.y) / (float) (c.y - a.y); // meow
+      // Common denominator (see Cramer's rule)
+      float den = (float) ab.x * (float) ac.y - (float) ac.x * (float) ab.y;
 
-      // Calculate the x-value we must have on the BC and CA lines
-      x0 = (int) ((float) b.x + bc_t * (float) (c.x - b.x));
-      x1 = (int) ((float) a.x + ca_t * (float) (c.x - a.x));
+      // Normalized barycentric coordinates
+      float v = (float) (ap.x * ac.y - ac.x * ap.y) / den;
+      float w = (float) (ab.x * ap.y - ap.x * ab.y) / den;
+      float u = 1.0f - v - w;
+
+      // If all components are nonnegative, we are in the triangle :)
+      if (u >= 0 && v >= 0 && w >= 0) {
+        image_pixel(image, x, y) = color;
+      }
     }
-
-    // Draw the x-span we computed
-    // This has the effect of filling this one row of the triangle
-    line(image, (vec2i_t) {x0, y}, (vec2i_t) {x1, y}, color);
   }
 }
 
@@ -163,7 +254,7 @@ int main(void) {
   triangle(&image, (vec2i_t) {314, 100}, (vec2i_t) {377, 378}, (vec2i_t) {231, 503}, (color_t) {.b = 255, .a = 255});
 
   // Try to write the output image
-  if (!stbi_write_png("output2.png", image.width, image.height, 4, image.pixels, 0)) {
+  if (!stbi_write_png("output3.png", image.width, image.height, 4, image.pixels, 0)) {
     fprintf(stderr, "error: failed to write output image\n");
     return 1;
   }
